0bd7b3310fb07bddcffc8c176093f3d62d7a354c
pramodrhegde <pramod.rh@gmail.com>
2015-11-14 17:01:45 +0530

    video-functionality

--- src/js/main.js
+++ src/js/main.js
@@ -39,7 +39,8 @@ $(document).ready(function(){
 			$this.siblings('.media').find('div.progresbar').css({'width':percentage_width+'%'});
 		})
 		.on('ended',function(){
-			$this.siblings('.media').find('.play-button').text('Replay');
+			$this.siblings('.media').find('.play-button').find('i:visible').toggleClass('hidden');
+			$this.siblings('.media').find('.play-button').find('i.fa-repeat').toggleClass('hidden');
 		});
 		return false;
 	});
@@ -47,54 +48,66 @@ $(document).ready(function(){
 	//Play Pause functionality
 	$('.play-button').on('click',function(){
 		var $this = $(this);
-		var video_object = $this.parent().siblings().get(0);
+		var video_object = $this.parents('.video-controls').siblings().get(0);
 		play_pasue(video_object,$this);
 	});
 
 	//Mute functionality
 	$('.mute-button').on('click',function(){
 		$this = $(this);
-		var video_object = $this.parent().siblings().get(0);
+		var video_object = $this.parents('.video-controls').siblings().get(0);
 		mute_unmute(video_object,$this);
 	});
 
 	//Volume functionality
+	var opacity = 1;
 	$('.volume-up-button,.volume-down-button').on('click',function(){
 		var $this = $(this);
 		var direction = $this.data('direction');
-		var video_object = $this.parent().siblings().get(0);
-		volume_updown(video_object,direction);
+		var video_object = $this.parents('.video-controls').siblings().get(0);
+		volume_updown(video_object,direction,$this);
+		if(direction==="up"){
+			opacity+=opacity==1?0:0.03;
+			$this.css({'opacity':opacity});
+		}else{
+			opacity-=opacity<=0.75?0:0.03;
+			$this.parent().find('.volume-up-button').css({'opacity':opacity});
+		}
 	});
 });
 
 function play_pasue(video,button){
 	if(video.paused){
 		video.play();
-		button.text('Pause');
+		button.find('i:visible').toggleClass('hidden');
+		button.find('i.fa-pause').toggleClass('hidden');
 	}else if(video.ended){
 
 		reset_player(video);
 		button.siblings('.progresbar').css({'width':0+'%'});
 		video.play();
-		button.text('Pause');
+		button.find('i:visible').toggleClass('hidden');
+		button.find('i.fa-pause').toggleClass('hidden');
 	}
 	else{
 		video.pause();
-		button.text('Play');
+		button.find('i:visible').toggleClass('hidden');
+		button.find('i.fa-play').toggleClass('hidden');
 	}
 }
 
 function mute_unmute(video,button){
 	if(video.muted){
 		video.muted= !video.muted;
-		button.text('Mute');
+		button.css({'opacity':1});
 	}else{
 		video.muted= !video.muted;
-		button.text('Unmute');
+		button.css({'opacity':0.75});
 	}
 }
 
 function volume_updown(video,direction){
+	
 	if(direction==="up"){
 		video.volume += video.volume == 1? 0:0.1;
 	}else{
@@ -117,7 +130,7 @@ function buffer_status(){
 
 		$(currentPlaying).parent('.media').find('.bufferbar').css({'width':percentageBuffer+'%'});
 		if(maxDuration == bufferedData){
-			alert('buffer-colpete');
+			//alert('buffer-colpete');
 			videosPlaying.splice(i,1);
 		}
 	}
